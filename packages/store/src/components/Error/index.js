bb58cce8adcfb0628add761fc65da6218f075ae2
barrypeng6 <barrypeng6@gmail.com>
2018-07-26 10:39:06 +0800

    fix(store): fix error compoenent (#45)
    * fix(store): fix error compoenent
    
    * fix(store): fix paddingtop of image

--- packages/store/src/components/Error/index.js
+++ packages/store/src/components/Error/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import oopsImg from './images/oops.png';
 
-const getErrorMsg = error => {
-  if (!error) return '';
-  if (error.message.match(/token verify failed/g)) {
+const getErrorMsg = _error => {
+  if (!_error) return '';
+
+  let error;
+  if (typeof _error === 'object') {
+    error = _error.message;
+  } else if (typeof _error === 'string') {
+    error = _error;
+  } else {
+    error = '';
+  }
+
+  if (error.match(/token verify failed/gm)) {
     return 'Store not found.';
   }
   return 'Something went wrong.';
@@ -12,29 +22,32 @@ const getErrorMsg = error => {
 export default (
   { error, errorInfo }, // eslint-disable-line
 ) => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      color: '#9e9e9e',
-      fontSize: '24px',
-    }}
-  >
-    <img style={{ paddingTop: '30%', width: 110 }} src={oopsImg} alt="oops" />
-    <div style={{ textAlign: 'center' }}>
-      <div>{getErrorMsg(error)}</div>
-      {errorInfo ? (
-        <details
-          style={{ whiteSpace: 'pre-wrap', textAlign: 'left', fontSize: 14 }}
-        >
-          {error && error.toString()}
-          <br />
-          {errorInfo.componentStack}
-        </details>
-      ) : (
-        <div style={{ fontSize: '14px' }}>{`(${error})`}</div>
-      )}
+  <React.Fragment>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: '#9e9e9e',
+        fontSize: '24px',
+      }}
+    >
+      <img style={{ paddingTop: 150, width: 110 }} src={oopsImg} alt="oops" />
+      <div style={{ textAlign: 'center' }}>
+        <div>{getErrorMsg(error)}</div>
+        {errorInfo ? (
+          <details
+            open
+            style={{ whiteSpace: 'pre-wrap', textAlign: 'left', fontSize: 14 }}
+          >
+            {error && error.toString()}
+            <br />
+            {errorInfo.componentStack}
+          </details>
+        ) : (
+          <div style={{ fontSize: '14px' }}>{`(${error})`}</div>
+        )}
+      </div>
     </div>
-  </div>
+  </React.Fragment>
 );
