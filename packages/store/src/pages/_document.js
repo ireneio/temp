3e2ecc3ef430e44ce7f32e768b64d79fed7aca3e
barrypeng6 <barrypeng6@gmail.com>
2018-07-27 18:10:40 +0800

    fix(store): add fb-root div (#69)

--- packages/store/src/pages/_document.js
+++ packages/store/src/pages/_document.js
@@ -43,6 +43,7 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
+          <div id="fb-root" />
           <Main />
           <script
             dangerouslySetInnerHTML={{
