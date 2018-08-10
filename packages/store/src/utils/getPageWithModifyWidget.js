6ad91304b20a0df1fe03897e0085b7733228824e
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2018-08-10 14:47:27 +0800

    fix(store): fix block data when using default group (#115)

--- packages/store/src/utils/getPageWithModifyWidget.js
+++ packages/store/src/utils/getPageWithModifyWidget.js
@@ -13,8 +13,8 @@ export default async function(page, XMeepshopDomain, query, cookie) {
   blocks = await Promise.all(
     blocks.map(async ({ width, componentWidth, widgets, ...block }) => ({
       ...block,
-      width: [0, null].includes(width) ? 100 : width,
-      componentWidth: componentWidth === null ? 0 : componentWidth,
+      width: [0, null, undefined].includes(width) ? 100 : width,
+      componentWidth: componentWidth || 0,
       // 整理及過濾Server-side rendering時的module資料，未來有可能在api server就幫前端整理好
       widgets: await modifyWidgetDataInServer(
         widgets,
