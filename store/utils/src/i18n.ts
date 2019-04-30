4a146acf5171f7f5e6df9b58b26a183b52dbe09a
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2019-04-30 17:47:29 +0800

    (@store/utils) Add new packages (#491)
    * feat(@store/utils): add new packages
    
    * fix(@store/utils): add vi_VN

---
+++ store/utils/src/i18n.ts
@@ -0,0 +1,24 @@
+// typescript import
+import i18next from 'i18next';
+
+// import
+import NextI18Next from 'next-i18next';
+
+// typescript definition
+export interface I18nPropsType {
+  t: (key: string) => string;
+  i18n: i18next.i18n & {
+    language: 'zh_TW' | 'en_US' | 'ja_JP' | 'vi_VN';
+  };
+}
+
+// definition
+const i18n = new NextI18Next({
+  defaultLanguage: 'zh_TW',
+  otherLanguages: ['en_US', 'ja_JP', 'vi_VN'],
+  localePath: 'src/static/locales',
+});
+
+export const { withNamespaces, appWithTranslation } = i18n;
+
+export default i18n;
