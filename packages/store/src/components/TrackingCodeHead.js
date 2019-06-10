f8791c033c7fb522b9bf441e15533699d0829d8c
piovischioh <piovischioh@gmail.com>
2019-06-10 13:55:59 +0800

    【個別店家】 Facebook 無法登入：應用程式未設定 (cname：imint)  (#557)

--- packages/store/src/components/TrackingCodeHead.js
+++ packages/store/src/components/TrackingCodeHead.js
@@ -37,7 +37,7 @@ export default class extends React.Component {
           appId: fbAppId,
           cookie: true,
           xfbml: true,
-          version: 'v3.0',
+          version: 'v3.3',
         });
         window.meepShopStore.fbSdkIsInstalled = true;
         FB.AppEvents.logPageView();
