7f293b46b0e62df7bdc92548a363fd5ea7343522
piovischioh <piovischioh@gmail.com>
2019-08-20 15:45:59 +0800

    【BUG】新架構/IE 瀏覽器 - 前台顯示異常 (#665)
    * fix(@meepshop/store): transpile next-i18next for ie
    
    * fix(@meepshop/store): fix lint
    
    * fix(@admin/server): transpile next-i18next for ie

--- admin/server/next.config.js
+++ admin/server/next.config.js
@@ -6,6 +6,7 @@ const withTypescript = require('@zeit/next-typescript');
 const withLess = require('@zeit/next-less');
 const withImages = require('next-images');
 const withSourceMaps = require('@zeit/next-source-maps');
+const withTM = require('next-transpile-modules');
 
 // fix: prevents error when .css files are required by node
 if (typeof require !== 'undefined') {
@@ -17,70 +18,73 @@ module.exports = withSourceMaps(
   withTypescript(
     withImages(
       withLess(
-        withBundleAnalyzer({
-          pageExtensions: ['js', 'ts', 'tsx'],
-          useFileSystemPublicRoutes: false,
-          publicRuntimeConfig: {
-            API_HOST: process.env.API_HOST || 'https://api.stage.meepcloud.com',
-            VERSION: process.env.REPO_VERSION || +new Date(),
-          },
-          analyzeServer: ['server', 'both'].includes(
-            process.env.BUNDLE_ANALYZE,
-          ),
-          analyzeBrowser: ['browser', 'both'].includes(
-            process.env.BUNDLE_ANALYZE,
-          ),
-          bundleAnalyzerConfig: {
-            server: {
-              analyzerMode: 'static',
-              reportFilename: '../../../analyzer/server.html',
+        withBundleAnalyzer(
+          withTM({
+            pageExtensions: ['js', 'ts', 'tsx'],
+            useFileSystemPublicRoutes: false,
+            publicRuntimeConfig: {
+              API_HOST:
+                process.env.API_HOST || 'https://api.stage.meepcloud.com',
+              VERSION: process.env.REPO_VERSION || +new Date(),
             },
-            browser: {
-              analyzerMode: 'static',
-              reportFilename: '../../analyzer/client.html',
+            analyzeServer: ['server', 'both'].includes(
+              process.env.BUNDLE_ANALYZE,
+            ),
+            analyzeBrowser: ['browser', 'both'].includes(
+              process.env.BUNDLE_ANALYZE,
+            ),
+            bundleAnalyzerConfig: {
+              server: {
+                analyzerMode: 'static',
+                reportFilename: '../../../analyzer/server.html',
+              },
+              browser: {
+                analyzerMode: 'static',
+                reportFilename: '../../analyzer/client.html',
+              },
             },
-          },
-          lessLoaderOptions: {
-            javascriptEnabled: true,
-          },
-          /* eslint-disable no-param-reassign */
-          webpack: (config, { dev }) => {
-            if (!dev) config.devtool = 'source-map';
+            lessLoaderOptions: {
+              javascriptEnabled: true,
+            },
+            /* eslint-disable no-param-reassign */
+            webpack: (config, { dev }) => {
+              if (!dev) config.devtool = 'source-map';
 
-            config.module.rules.find(
-              ({ use: { loader } }) => loader === 'next-babel-loader',
-            ).use.options.configFile = path.resolve(
-              __dirname,
-              '../../babel.config.js',
-            );
-            config.plugins.push(new MomentLocalesPlugin());
+              config.module.rules.find(
+                ({ use: { loader } }) => loader === 'next-babel-loader',
+              ).use.options.configFile = path.resolve(
+                __dirname,
+                '../../babel.config.js',
+              );
+              config.plugins.push(new MomentLocalesPlugin());
 
-            const originalEntry = config.entry;
+              const originalEntry = config.entry;
 
-            config.entry = async () => {
-              const entries = await originalEntry();
+              config.entry = async () => {
+                const entries = await originalEntry();
 
-              // fix for IE
-              [
-                'core-js/modules/es6.string.starts-with',
-                'core-js/modules/es6.object.assign',
-                'core-js/modules/es6.symbol',
-                'core-js/modules/es6.array.from',
-              ].forEach(polyfill => {
-                if (
-                  entries['main.js'] &&
-                  !entries['main.js'].includes(require.resolve(polyfill))
-                )
-                  entries['main.js'].unshift(require.resolve(polyfill));
-              });
+                // fix for IE
+                [
+                  'core-js/modules/es6.string.starts-with',
+                  'core-js/modules/es6.object.assign',
+                  'core-js/modules/es6.symbol',
+                  'core-js/modules/es6.array.from',
+                ].forEach(polyfill => {
+                  if (
+                    entries['main.js'] &&
+                    !entries['main.js'].includes(require.resolve(polyfill))
+                  )
+                    entries['main.js'].unshift(require.resolve(polyfill));
+                });
 
-              return entries;
-            };
+                return entries;
+              };
 
-            return config;
-          },
-          /* eslint-enable no-param-reassign */
-        }),
+              return config;
+            },
+            /* eslint-enable no-param-reassign */
+          }),
+        ),
       ),
     ),
   ),
--- packages/store/next.config.js
+++ packages/store/next.config.js
@@ -6,6 +6,7 @@ const withTypescript = require('@zeit/next-typescript');
 const withLess = require('@zeit/next-less');
 const withImages = require('next-images');
 const withSourceMaps = require('@zeit/next-source-maps');
+const withTM = require('next-transpile-modules');
 
 const FONT_FAMILY =
   '"PingFang TC", "Microsoft JhengHei", "Helvetica Neue", "Helvetica", "source-han-sans-traditional", "Arial", "sans-serif"';
@@ -26,77 +27,81 @@ module.exports = withSourceMaps(
   withTypescript(
     withImages(
       withLess(
-        withBundleAnalyzer({
-          useFileSystemPublicRoutes: false,
-          publicRuntimeConfig: {
-            API_HOST: process.env.API_HOST || 'https://api.stage.meepcloud.com',
-            EXTERNAL_API_HOST:
-              process.env.EXTERNAL_API_HOST ||
-              'https://api.stage.meepcloud.com',
-            STORE_DOMAIN: process.env.STORE_DOMAIN,
-            VERSION: process.env.REPO_VERSION || +new Date(),
-          },
-          analyzeServer: ['server', 'both'].includes(
-            process.env.BUNDLE_ANALYZE,
-          ),
-          analyzeBrowser: ['browser', 'both'].includes(
-            process.env.BUNDLE_ANALYZE,
-          ),
-          bundleAnalyzerConfig: {
-            server: {
-              analyzerMode: 'static',
-              reportFilename: '../../../analyzer/server.html',
+        withBundleAnalyzer(
+          withTM({
+            transpileModules: ['next-i18next'],
+            useFileSystemPublicRoutes: false,
+            publicRuntimeConfig: {
+              API_HOST:
+                process.env.API_HOST || 'https://api.stage.meepcloud.com',
+              EXTERNAL_API_HOST:
+                process.env.EXTERNAL_API_HOST ||
+                'https://api.stage.meepcloud.com',
+              STORE_DOMAIN: process.env.STORE_DOMAIN,
+              VERSION: process.env.REPO_VERSION || +new Date(),
             },
-            browser: {
-              analyzerMode: 'static',
-              reportFilename: '../../analyzer/client.html',
+            analyzeServer: ['server', 'both'].includes(
+              process.env.BUNDLE_ANALYZE,
+            ),
+            analyzeBrowser: ['browser', 'both'].includes(
+              process.env.BUNDLE_ANALYZE,
+            ),
+            bundleAnalyzerConfig: {
+              server: {
+                analyzerMode: 'static',
+                reportFilename: '../../../analyzer/server.html',
+              },
+              browser: {
+                analyzerMode: 'static',
+                reportFilename: '../../analyzer/client.html',
+              },
             },
-          },
-          lessLoaderOptions: {
-            javascriptEnabled: true,
-            modifyVars: {
-              'font-family': FONT_FAMILY,
-              'font-family-no-number': FONT_FAMILY,
+            lessLoaderOptions: {
+              javascriptEnabled: true,
+              modifyVars: {
+                'font-family': FONT_FAMILY,
+                'font-family-no-number': FONT_FAMILY,
+              },
             },
-          },
-          /* eslint-disable no-param-reassign */
-          webpack: (config, { dev }) => {
-            if (!dev) config.devtool = 'source-map';
+            /* eslint-disable no-param-reassign */
+            webpack: (config, { dev }) => {
+              if (!dev) config.devtool = 'source-map';
 
-            config.module.rules.find(
-              ({ use: { loader } }) => loader === 'next-babel-loader',
-            ).use.options.configFile = path.resolve(
-              __dirname,
-              '../../babel.config.js',
-            );
-            config.plugins.push(new MomentLocalesPlugin());
+              config.module.rules.find(
+                ({ use: { loader } }) => loader === 'next-babel-loader',
+              ).use.options.configFile = path.resolve(
+                __dirname,
+                '../../babel.config.js',
+              );
+              config.plugins.push(new MomentLocalesPlugin());
 
-            const originalEntry = config.entry;
+              const originalEntry = config.entry;
 
-            config.entry = async () => {
-              const entries = await originalEntry();
+              config.entry = async () => {
+                const entries = await originalEntry();
 
-              // fix for IE
-              [
-                'core-js/modules/es6.string.starts-with',
-                'core-js/modules/es6.object.assign',
-                'core-js/modules/es6.symbol',
-                'core-js/modules/es6.array.from',
-              ].forEach(polyfill => {
-                if (
-                  entries['main.js'] &&
-                  !entries['main.js'].includes(require.resolve(polyfill))
-                )
-                  entries['main.js'].unshift(require.resolve(polyfill));
-              });
+                // fix for IE
+                [
+                  'core-js/modules/es6.string.starts-with',
+                  'core-js/modules/es6.object.assign',
+                  'core-js/modules/es6.symbol',
+                  'core-js/modules/es6.array.from',
+                ].forEach(polyfill => {
+                  if (
+                    entries['main.js'] &&
+                    !entries['main.js'].includes(require.resolve(polyfill))
+                  )
+                    entries['main.js'].unshift(require.resolve(polyfill));
+                });
 
-              return entries;
-            };
+                return entries;
+              };
 
-            return config;
-          },
-          /* eslint-enable no-param-reassign */
-        }),
+              return config;
+            },
+            /* eslint-enable no-param-reassign */
+          }),
+        ),
       ),
     ),
   ),
