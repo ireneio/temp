a98a5f3118f098da1317f7ebc4bb9873872c3679
piovischioh <piovischioh@gmail.com>
2020-03-02 14:20:33 +0800

    (@meepshop/front-end): add kustomize (#847)
    * feat(@meepshop/front-end): add kustomize
    
    * feat(@meepshop/front-end): remove old k8s config
    
    * fix(@meepshop/front-end): modify deployment and virtual service
    
    * fix(@admin/server): remove virtual service
    
    * fix(root): fix relative path
    
    * fix(root): shell typo
    
    * fix(@meepshop/store): quote wildcards in kustomize
    
    Co-authored-by: BellaHsueh <35363009+BellaHsueh@users.noreply.github.com>

--- .circleci/bin/deploy_k8s.sh
+++ .circleci/bin/deploy_k8s.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)"
 
 VERSION=${CIRCLE_TAG:=${TEST_VERSION:=$CIRCLE_BRANCH}}
 PACKAGE_NAME=$(node ../../.circleci/bin/getPackageName.js)
+ISTIOCTL_VERSION=1.3.6
 
 # check need publish
 if [ $(node ../../.circleci/bin/checkNeedPublish.js) != true ]; then
@@ -14,34 +20,48 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/l
 chmod +x ./kubectl
 mv ./kubectl /tmp/
 
-# generate kubernetes config file
+# install istioctl
+curl -sLO https://github.com/istio/istio/releases/download/${ISTIOCTL_VERSION}/istio-${ISTIOCTL_VERSION}-linux.tar.gz
+tar zxf istio-${ISTIOCTL_VERSION}-linux.tar.gz
+sudo mv istio-${ISTIOCTL_VERSION}/bin/istioctl /usr/local/bin/istioctl
+rm istio-${ISTIOCTL_VERSION}-linux.tar.gz
+rm -r istio-${ISTIOCTL_VERSION}
+
+curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases |\
+ grep browser_download |\
+ grep linux |\
+ grep kustomize_kustomize |\
+ head -n1 |\
+ cut -d '"' -f 4 |\
+ xargs curl -O -L
+sudo mv kustomize_kustomize.*_linux_amd64 /usr/local/bin/kustomize
+chmod u+x /usr/local/bin/kustomize
+
+# kubernetes config
 /tmp/kubectl config set-cluster default-cluster \
        --insecure-skip-tls-verify=true \
-       --server=https://$KUBERNETES_PUBLIC_ADDRESS:443 \
-       --kubeconfig=/tmp/kubeconfig
+       --server=https://$KUBERNETES_PUBLIC_ADDRESS:443
+
 /tmp/kubectl config set-credentials $KUBERNETES_USER \
     --username=$KUBERNETES_USER \
-    --password=$KUBERNETES_PASSWD \
-    --kubeconfig=/tmp/kubeconfig
+    --password=$KUBERNETES_PASSWD
+
 /tmp/kubectl config set-context default-cluster \
     --cluster=default-cluster \
-    --user=$KUBERNETES_USER \
-    --kubeconfig=/tmp/kubeconfig
-/tmp/kubectl config use-context default-cluster --kubeconfig=/tmp/kubeconfig
+    --user=$KUBERNETES_USER
 
-# replace deploy.yml version with git commit sha
-sed -i "s/{{ build.tag }}/$VERSION/" ./k8s_config/deploy.yaml
+/tmp/kubectl config use-context default-cluster
 
-# kubernetes deploy
-/tmp/kubectl apply --force -f ./k8s_config/deploy.yaml --kubeconfig=/tmp/kubeconfig
-/tmp/kubectl apply --force -f ./k8s_config/svc.yaml --kubeconfig=/tmp/kubeconfig
-/tmp/kubectl apply --force -f ./k8s_config/cm_stage.yaml --kubeconfig=/tmp/kubeconfig
+# kustomize deployment
+cd ./kustomize/overlays/staging
+kustomize edit set image asia.gcr.io/instant-matter-785/${PACKAGE_NAME}=asia.gcr.io/instant-matter-785/${PACKAGE_NAME}:${VERSION}
+kustomize build . | istioctl kube-inject -f - | /tmp/kubectl apply -f -
 
 if [[ $CIRCLE_TAG =~ ^v[0-9.]*$ ]]; then
   # release bot
   curl -H 'Content-Type: application/json' -d "{
     \"icon\": \"https://res.cloudinary.com/cakeresume/image/upload/s--Lv6sj1oB--/c_pad,fl_png8,h_200,w_200/v1509504375/pcotebjqdkfuqbvbt4xc.png\",
-    \"activity\": \"release bot\",
+    \"activity\": \"Release Bot\",
     \"title\": \"$PACKAGE_NAME: $VERSION\"
   }" https://hooks.glip.com/webhook/57916ea2-14b4-488c-8cb9-95efe808cae2
 fi
