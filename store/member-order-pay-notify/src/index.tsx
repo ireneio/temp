f50990b1a89c8cae642b827e74e911eaa4a02b55
happycat6323 <happycat6323@yahoo.com.tw>
2020-03-03 13:50:20 +0800

    【優化】前台 / 匯款通知 / 內容變更行為 (#866)
    * feat(@meepshop/sttore/@store): modify order pay notify validate
    
    * feat(@meepshop/store/@store): modify send order pay notify error modal
    
    * feat(@store/member-order-pay-notify): use hooks
    
    * fix(@store/member-order-pay-notify): fix validateFields
    
    * feat(@store/member-order-pay-notify): modify component construction
    
    * feat(@store/member-order-pay-notify): modify component construction

--- store/member-order-pay-notify/src/index.tsx
+++ store/member-order-pay-notify/src/index.tsx
@@ -1,211 +1,160 @@
 // typescript import
-import { I18nPropsType } from '@store/utils/lib/i18n';
-import { MutationFunction } from '@apollo/react-common';
+import { FormComponentProps } from 'antd/lib/form';
 
 // import
-import React from 'react';
-import { Query, Mutation } from '@apollo/react-components';
+import React, { useState } from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import { Spin, Icon, Input, Button, Modal, notification } from 'antd';
+import { Spin, Icon, Form, Input, Button, Modal, notification } from 'antd';
 
-import { withTranslation } from '@store/utils/lib/i18n';
+import { useTranslation } from '@store/utils/lib/i18n';
 
 import { DEFAULT_MESSAGE } from './constants';
 import styles from './styles/index.less';
-
-// graphql typescript
-import {
-  getMemberOrderPayNotify,
-  getMemberOrderPayNotify_viewer_order as getMemberOrderPayNotifyViewerOrder,
-  getMemberOrderPayNotify_getColorList as getMemberOrderPayNotifyGetColorList,
-} from './__generated__/getMemberOrderPayNotify';
-import { sendOrderPayNotify as sendOrderPayNotifyType } from './__generated__/sendOrderPayNotify';
 
 // graphql import
 import { colorListFragment } from '@store/apollo-client-resolvers/lib/ColorList';
 
 // typescript definition
-interface PropsType extends I18nPropsType {
-  paidMessage: string;
-  order: getMemberOrderPayNotifyViewerOrder;
-  colors: getMemberOrderPayNotifyGetColorList['colors'];
+interface PropsType extends FormComponentProps {
+  orderId: string;
 }
 
 // definition
-const { TextArea } = Input;
+const query = gql`
+  query getOrderPaidMessage($orderId: ID!) {
+    viewer {
+      id
+      store {
+        id
+        setting {
+          paidMessage
+        }
+      }
+      order(orderId: $orderId) {
+        id
+        orderNo
+        paidMessage {
+          note
+        }
+      }
+    }
 
-class MemberOrderPayNotify extends React.PureComponent<PropsType> {
-  public state = {
-    message:
-      // eslint-disable-next-line react/destructuring-assignment
-      (this.props.order?.paidMessage?.slice(-1)[0] || {}).note ||
-      // eslint-disable-next-line react/destructuring-assignment
-      this.props.paidMessage ||
-      DEFAULT_MESSAGE,
-    disabled:
-      // eslint-disable-next-line react/destructuring-assignment
-      (this.props.order?.paidMessage?.length || 0) > 0,
-  };
-
-  private send = (
-    sendOrderPayNotify: MutationFunction<sendOrderPayNotifyType>,
-  ): void => {
-    const {
-      // HOC
-      t,
-      paidMessage: customDefaultMessage,
-
-      // props
-      order: { id, paidMessage },
-    } = this.props;
-    const { message } = this.state;
-
-    const prevMessage =
-      ((paidMessage || []).slice(-1)[0] || { note: '' }).note ||
-      customDefaultMessage ||
-      DEFAULT_MESSAGE;
-
-    if (message !== prevMessage)
-      sendOrderPayNotify({
-        variables: {
-          newMessage: {
-            id,
-            paidMessage: {
-              createData: { note: message },
-            },
-          },
-        },
-      });
-    else
-      Modal.error({
-        title: t('pleaseEditPayNotification'),
-        maskClosable: true,
-      });
-  };
-
-  private updateMessages = (): void => {
-    const { t } = this.props;
-
-    notification.success({ message: t('sendSuccess') });
-    this.setState({ disabled: true });
-  };
-
-  public render(): React.ReactNode {
-    const {
-      // HOC
-      t,
-
-      // props
-      order: { orderNo },
-      colors,
-    } = this.props;
-    const { message, disabled } = this.state;
-
-    return (
-      <div className={styles.root}>
-        <div>
-          <h1>
-            {t('orderNo')}
-            {orderNo}
-          </h1>
-
-          <TextArea
-            value={message}
-            onChange={({ target: { value } }) =>
-              this.setState({ message: value })
-            }
-            disabled={disabled}
-            rows={12}
-          />
-
-          <div>
-            <Mutation<sendOrderPayNotifyType>
-              mutation={gql`
-                mutation sendOrderPayNotify($newMessage: [UpdateOrder]) {
-                  updateOrderList(updateOrderList: $newMessage) {
-                    id
-                    paidMessage {
-                      note
-                    }
-                  }
-                }
-              `}
-              update={this.updateMessages}
-            >
-              {sendOrderPayNotify => (
-                <Button
-                  size="large"
-                  onClick={() =>
-                    disabled
-                      ? this.setState({ disabled: false })
-                      : this.send(sendOrderPayNotify)
-                  }
-                  style={{ color: colors[3], borderColor: colors[3] }}
-                >
-                  {t(disabled ? 'editAgain' : 'send')}
-                </Button>
-              )}
-            </Mutation>
-          </div>
-        </div>
-      </div>
-    );
+    getColorList {
+      ...colorListFragment
+    }
   }
-}
 
-const EnhancedMemberOrderPayNotify = withTranslation('member-order-pay-notify')(
-  MemberOrderPayNotify,
-);
+  ${colorListFragment}
+`;
 
-export default React.memo(({ orderId }: { orderId: string }) => (
-  <Query<getMemberOrderPayNotify>
-    query={gql`
-      query getMemberOrderPayNotify($orderId: ID!) {
-        viewer {
-          id
-          store {
+export default Form.create<PropsType>()(
+  React.memo(({ orderId, form }: PropsType) => {
+    const { t } = useTranslation('member-order-pay-notify');
+
+    const { loading, data } = useQuery(query, { variables: { orderId } });
+
+    const [disabledEdit, setDisabledEdit] = useState(
+      Boolean(data?.viewer.order.paidMessage?.length),
+    );
+
+    const [updateOrderPaidMessage] = useMutation(
+      gql`
+        mutation updateOrderPaidMessage($newPaidMessage: [UpdateOrder]) {
+          updateOrderList(updateOrderList: $newPaidMessage) {
             id
-            setting {
-              paidMessage
-            }
-          }
-          order(orderId: $orderId) {
-            id
-            orderNo
             paidMessage {
               note
             }
           }
         }
+      `,
+      {
+        update: () => {
+          notification.success({ message: t('send-success') });
+          setDisabledEdit(true);
+        },
+      },
+    );
 
-        getColorList {
-          ...colorListFragment
-        }
-      }
+    if (loading || !data)
+      return <Spin indicator={<Icon type="loading" spin />} />;
 
-      ${colorListFragment}
-    `}
-    variables={{ orderId }}
-  >
-    {({ loading, error, data }) => {
-      if (loading || error || !data)
-        return <Spin indicator={<Icon type="loading" spin />} />;
+    const {
+      viewer: {
+        store: {
+          setting: { paidMessage: customDefaultMessage },
+        },
+        order: { id, orderNo, paidMessage },
+      },
+      getColorList: { colors },
+    } = data;
 
-      const { viewer, getColorList } = data;
+    const { getFieldDecorator, getFieldError, validateFields } = form;
 
-      const order = viewer?.order;
-      const paidMessage = viewer?.store?.setting?.paidMessage || '';
+    return (
+      <Form className={styles.root}>
+        <div>
+          <h1>
+            {t('order-no')}
+            {orderNo}
+          </h1>
 
-      if (!order || !getColorList)
-        return <Spin indicator={<Icon type="loading" spin />} />;
+          <Form.Item>
+            {getFieldDecorator('message', {
+              initialValue:
+                paidMessage?.slice(-1)[0]?.note ||
+                customDefaultMessage ||
+                DEFAULT_MESSAGE,
+              rules: [
+                { required: true, message: t('please-enter-paid-message') },
+              ],
+            })(<Input.TextArea disabled={disabledEdit} rows={12} />)}
+          </Form.Item>
 
-      return (
-        <EnhancedMemberOrderPayNotify
-          paidMessage={paidMessage}
-          order={order}
-          colors={getColorList.colors}
-        />
-      );
-    }}
-  </Query>
-));
+          <div>
+            <Button
+              size="large"
+              style={{ color: colors[3], borderColor: colors[3] }}
+              disabled={Boolean(getFieldError('message'))}
+              onClick={() => {
+                if (disabledEdit) setDisabledEdit(false);
+                else
+                  validateFields((errors, values) => {
+                    if (!errors) {
+                      const { message } = values;
+                      const prevMessage =
+                        paidMessage?.slice(-1)[0]?.note ||
+                        customDefaultMessage ||
+                        DEFAULT_MESSAGE;
+
+                      if (message !== prevMessage)
+                        updateOrderPaidMessage({
+                          variables: {
+                            newPaidMessage: {
+                              id,
+                              paidMessage: {
+                                createData: { note: message },
+                              },
+                            },
+                          },
+                        });
+                      else
+                        Modal.error({
+                          title: t('pay-message-is-not-updated'),
+                          content: <>{t('please-edit-paid-message')}</>,
+                          okText: t('confirm'),
+                          maskClosable: true,
+                        });
+                    }
+                  });
+              }}
+            >
+              {t(disabledEdit ? 'edit-again' : 'send')}
+            </Button>
+          </div>
+        </div>
+      </Form>
+    );
+  }),
+);
