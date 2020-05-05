42646d340d66e51c8ac302b9fd5d292a3844ae5f
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2020-05-05 15:26:09 +0800

    (@meepshop/frontend) Add install.sh to ci (#938)
    * ci(@meepshop/frontend): add install.sh
    
    * ci(@meepshop/frontend): add more set

--- .circleci/bin/deploy_k8s.sh
+++ .circleci/bin/deploy_k8s.sh
@@ -1,56 +1,17 @@
 #!/usr/bin/env bash
+
 set -o errexit
 set -o pipefail
 set -o nounset
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)"
 
 VERSION=${CIRCLE_TAG:=${TEST_VERSION:=$CIRCLE_BRANCH}}
 PACKAGE_NAME=$(node ../../.circleci/bin/getPackageName.js)
-ISTIOCTL_VERSION=1.5.0
 
 # check need publish
 if [ $(node ../../.circleci/bin/checkNeedPublish.js) != true ]; then
   echo "not need to publish: $VERSION"
   exit
 fi
-
-# install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv ./kubectl /tmp/
-
-# install istioctl
-curl -sLO https://github.com/istio/istio/releases/download/${ISTIOCTL_VERSION}/istio-${ISTIOCTL_VERSION}-linux.tar.gz
-tar zxf istio-${ISTIOCTL_VERSION}-linux.tar.gz
-sudo mv istio-${ISTIOCTL_VERSION}/bin/istioctl /usr/local/bin/istioctl
-rm istio-${ISTIOCTL_VERSION}-linux.tar.gz
-rm -r istio-${ISTIOCTL_VERSION}
-
-curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases |\
- grep browser_download |\
- grep linux |\
- grep kustomize_kustomize |\
- head -n1 |\
- cut -d '"' -f 4 |\
- xargs curl -O -L
-sudo mv kustomize_kustomize.*_linux_amd64 /usr/local/bin/kustomize
-chmod u+x /usr/local/bin/kustomize
-
-# kubernetes config
-/tmp/kubectl config set-cluster default-cluster \
-       --insecure-skip-tls-verify=true \
-       --server=https://$KUBERNETES_PUBLIC_ADDRESS:443
-
-/tmp/kubectl config set-credentials $KUBERNETES_USER \
-    --username=$KUBERNETES_USER \
-    --password=$KUBERNETES_PASSWD
-
-/tmp/kubectl config set-context default-cluster \
-    --cluster=default-cluster \
-    --user=$KUBERNETES_USER
-
-/tmp/kubectl config use-context default-cluster
 
 # kustomize deployment
 cd ./kustomize/overlays/staging
---
+++ .circleci/bin/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+ISTIOCTL_VERSION=1.5.0
+
+# install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /tmp/
+
+# install istioctl
+curl -sLO https://github.com/istio/istio/releases/download/${ISTIOCTL_VERSION}/istio-${ISTIOCTL_VERSION}-linux.tar.gz
+tar zxf istio-${ISTIOCTL_VERSION}-linux.tar.gz
+sudo mv istio-${ISTIOCTL_VERSION}/bin/istioctl /usr/local/bin/istioctl
+rm istio-${ISTIOCTL_VERSION}-linux.tar.gz
+rm -r istio-${ISTIOCTL_VERSION}
+
+curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases |\
+ grep browser_download |\
+ grep linux |\
+ grep kustomize_kustomize |\
+ head -n1 |\
+ cut -d '"' -f 4 |\
+ xargs curl -O -L
+sudo mv kustomize_kustomize.*_linux_amd64 /usr/local/bin/kustomize
+chmod u+x /usr/local/bin/kustomize
+
+# kubernetes config
+/tmp/kubectl config set-cluster default-cluster \
+       --insecure-skip-tls-verify=true \
+       --server=https://$KUBERNETES_PUBLIC_ADDRESS:443
+
+/tmp/kubectl config set-credentials $KUBERNETES_USER \
+    --username=$KUBERNETES_USER \
+    --password=$KUBERNETES_PASSWD
+
+/tmp/kubectl config set-context default-cluster \
+    --cluster=default-cluster \
+    --user=$KUBERNETES_USER
+
+/tmp/kubectl config use-context default-cluster
