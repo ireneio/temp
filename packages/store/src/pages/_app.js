a32cefe52f2d1a5d5f19590b01fede50081ea5e7
piovischioh <piovischioh@gmail.com>
2020-08-13 11:32:26 +0800

    【優化】FB、IG 的 in-app-browser - 無法兩指放大頁面 (#1100)

--- packages/store/src/pages/_app.js
+++ packages/store/src/pages/_app.js
@@ -259,7 +259,7 @@ class MyApp extends App {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
+            content="width=device-width, initial-scale=1, maximum-scale=1.001"
           />
           <meta name="format-detection" content="telephone=no" />
         </Head>
