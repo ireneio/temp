aafc9227b10b63f73e7761535d17bbf06424ab69
LexiWu <sakura9641@gmail.com>
2021-07-13 15:37:30 +0800

    antd custom datepicker component

---
+++ meepshop/date-picker/mock.ts
@@ -0,0 +1,6 @@
+// definition
+export default {
+  placeholder: 'hello',
+  defaultValue: new Date('2021/07/12'),
+  size: 'large' as const,
+};
---
+++ meepshop/date-picker/src/index.ts
@@ -0,0 +1,6 @@
+// import
+import generatePicker from 'antd/lib/date-picker/generatePicker';
+import dateFnsGenerateConfig from 'rc-picker/lib/generate/dateFns';
+
+// definition
+export default generatePicker<Date>(dateFnsGenerateConfig);
