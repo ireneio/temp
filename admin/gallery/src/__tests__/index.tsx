73d3b8720540757a8107d8d9b6923070f15058b4
LexiWu <sakura9641@gmail.com>
2021-10-19 14:51:45 +0800

    內部開單 - 新增 @admin/gallery package (#1819)

---
+++ admin/gallery/src/__tests__/index.tsx
@@ -0,0 +1,9 @@
+// import
+import React from 'react';
+
+import runTest from '@meepshop/mock-types/src/runTest';
+
+import Gallery from '../index';
+
+// definition
+runTest('admin', <Gallery />);
---
+++ admin/gallery/src/index.tsx
@@ -0,0 +1,13 @@
+// typescript import
+
+// import
+import React from 'react';
+
+// graphql typescript
+
+// graphql import
+
+// typescript definition
+
+// definition
+export default React.memo(() => <div />);
