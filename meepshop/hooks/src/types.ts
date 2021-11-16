e1034d94a903c60584339439105de0bf520fa391
Cate Wang <happycat6323@yahoo.com.tw>
2021-11-16 15:41:38 +0800

    (@meepshop/hooks) Add usePrevious (#1876)

--- meepshop/hooks/src/types.ts
+++ meepshop/hooks/src/types.ts
@@ -3,6 +3,7 @@ export { default as useAutoLinker } from './useAutoLinker';
 export { default as useClipboard } from './useClipboard';
 export { default as useEcpay } from './useEcpay';
 export { default as useFbParse } from './useFbParse';
+export { default as usePrevious } from './usePrevious';
 
 // definition
 throw new Error('Do not import');
