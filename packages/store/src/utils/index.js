e0e0861bcdace2c4215325b816eafe8710cfd1df
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2021-12-03 17:54:05 +0800

    【優化】移除 next-store 沒有使用套件 (#1952)
    * refactor(@meepshop/next-store): remove getIn
    
    * refactor(@meepshop/next-store): remove not used packages

--- packages/store/src/utils/index.js
+++ packages/store/src/utils/index.js
@@ -1,8 +1,7 @@
 import getStateError from './getStateError';
 import goTo from './goTo';
-import getIn from './getIn';
 import uriParser from './uriParser';
 import getReqArgs from './getReqArgs';
 import getJoinedModule from './getJoinedModule';
 
-export { getStateError, goTo, getIn, uriParser, getReqArgs, getJoinedModule };
+export { getStateError, goTo, uriParser, getReqArgs, getJoinedModule };
