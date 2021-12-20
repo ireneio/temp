be3c57da6f6a60cd878ffc81d494fb7751741e6b
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2021-12-20 16:02:06 +0800

    內部開單 - 在 next-store 加 log 看是哪個 query operation (#1995)

--- meepshop/apollo/src/utils/errorLink.ts
+++ meepshop/apollo/src/utils/errorLink.ts
@@ -24,43 +24,52 @@ export default (
   errorFilter: errorFilterType,
   logger: loggerType,
 ): ReturnType<typeof onError> =>
-  onError(({ response, graphQLErrors, networkError }) => {
-    const errorLog =
-      typeof window === 'undefined'
-        ? logger.error
-        : ({ name, ...message }: { name: string }) =>
-            notification.error({
-              message: 'Error!',
-              description: `[${name} error]: ${JSON.stringify(message)}`,
-            });
+  onError(
+    ({
+      response,
+      graphQLErrors,
+      networkError,
+      operation: { operationName },
+    }) => {
+      const errorLog =
+        typeof window === 'undefined'
+          ? logger.error
+          : ({ name, ...message }: { name: string }) =>
+              notification.error({
+                message: 'Error!',
+                description: `[${name} error]: ${JSON.stringify(message)}`,
+              });
 
-    if (graphQLErrors) {
-      const errors = graphQLErrors.filter(errorFilter);
+      if (graphQLErrors) {
+        const errors = graphQLErrors.filter(errorFilter);
 
-      if (response && errors.length === 0) {
-        response.errors = undefined;
-        return;
+        if (response && errors.length === 0) {
+          response.errors = undefined;
+          return;
+        }
+
+        errors.forEach(({ message, locations, path }) => {
+          errorLog({
+            name: 'GraphQL',
+            operationName,
+            message,
+            locations,
+            path,
+          });
+        });
       }
 
-      errors.forEach(({ message, locations, path }) => {
-        errorLog({
-          name: 'GraphQL',
-          message,
-          locations,
-          path,
-        });
+      if (
+        networkError &&
+        'statusCode' in networkError &&
+        [401, 403].includes(networkError.statusCode)
+      )
+        return;
+
+      errorLog({
+        name: 'Network',
+        operationName,
+        networkError,
       });
-    }
-
-    if (
-      networkError &&
-      'statusCode' in networkError &&
-      [401, 403].includes(networkError.statusCode)
-    )
-      return;
-
-    errorLog({
-      name: 'Network',
-      networkError,
-    });
-  });
+    },
+  );
