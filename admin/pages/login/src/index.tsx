e27003c7a13749caf353ba9e600cb9ac789b72d1
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2021-12-21 18:35:10 +0800

    T10182 - 【優化】Admin SEO：設定後台 SEO (#1985)
    T10182 - 【優化】Admin SEO：設定後台 SEO

--- admin/pages/login/src/index.tsx
+++ admin/pages/login/src/index.tsx
@@ -3,6 +3,7 @@ import { NextPage } from 'next';
 
 // import
 import React, { useState } from 'react';
+import Head from 'next/head';
 
 import { loginBackground } from '@meepshop/images';
 
@@ -28,12 +29,24 @@ const LoginWrapper: NextPage<PropsType> = React.memo(
     }[action];
 
     return (
-      <div
-        className={styles.root}
-        style={{ backgroundImage: `url('${loginBackground}')` }}
-      >
-        <Content setAction={setAction} />
-      </div>
+      <>
+        <Head>
+          <title>meepShop 後台登入</title>
+
+          <meta
+            key="description"
+            name="description"
+            content="最彈性的頁面設計、會員分級工具等，還有隱藏式賣場、一頁式商店、天眼等強大功能，全都不分放案，讓你輕鬆就能架設質感的品牌官網販售商品。更多詳情歡迎預約專人開店諮詢。"
+          />
+        </Head>
+
+        <div
+          className={styles.root}
+          style={{ backgroundImage: `url('${loginBackground}')` }}
+        >
+          <Content setAction={setAction} />
+        </div>
+      </>
     );
   },
 );
