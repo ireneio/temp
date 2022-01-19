41b7290368cde84e0aafec054e9a9d88b54e7b3b
Cate Wang <happycat6323@yahoo.com.tw>
2022-01-19 14:33:34 +0800

    內部開單 - 新增 @store/alert (#2082)
    * feat(@store/alert): add @store/alert
    
    * fix(@store/alert): fix code style

---
+++ store/alert/src/index.tsx
@@ -0,0 +1,23 @@
+// typescript import
+import { AlertProps } from 'antd/lib/alert';
+
+// import
+import React from 'react';
+import { Alert } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+
+import styles from './styles/index.less';
+
+// definition
+export default React.memo(({ type, ...props }: AlertProps) => (
+  <Alert
+    className={styles.root}
+    type={type}
+    icon={
+      ['warning', 'error'].includes(type || '') ? (
+        <ExclamationCircleOutlined />
+      ) : null
+    }
+    {...props}
+  />
+));
