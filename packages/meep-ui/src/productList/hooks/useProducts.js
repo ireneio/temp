79ccb3cd4330ed37495f21ac3122a0442458fe7d
Cate Wang <happycat6323@yahoo.com.tw>
2022-03-25 14:39:35 +0800

    fix(@meepshop/meep-ui): fix getProducts query (#2234)
    【個別店家】 置頂選單 - 前台無顯示正確品項

--- packages/meep-ui/src/productList/hooks/useProducts.js
+++ packages/meep-ui/src/productList/hooks/useProducts.js
@@ -45,7 +45,6 @@ export default ({
             },
           ],
           or: [],
-          not: [],
         },
         sort: [
           {
@@ -110,13 +109,6 @@ export default ({
         field: 'retailPrice',
         ...(!gte ? {} : { gte: parseFloat(gte) }),
         ...(!lte ? {} : { lte: parseFloat(lte) }),
-      });
-
-      output.search.filter.not.push({
-        type: 'exact',
-        child: 'variant',
-        field: 'status',
-        query: '0',
       });
     }
 
