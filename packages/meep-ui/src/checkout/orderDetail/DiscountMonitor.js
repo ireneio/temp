dbfacf2ef32762387d8115e780945a7b384aea49
piovischioh <piovischioh@gmail.com>
2022-04-21 16:16:18 +0800

    fix: DiscountMonitor trigger (#2272)
    【BUG】修正結帳頁折扣碼/購物金無法清空

--- packages/meep-ui/src/checkout/orderDetail/DiscountMonitor.js
+++ packages/meep-ui/src/checkout/orderDetail/DiscountMonitor.js
@@ -4,11 +4,11 @@ import React, { useEffect } from 'react';
 // definition
 export default React.memo(({ coupon, points, computeOrderList }) => {
   useEffect(() => {
-    if (coupon) computeOrderList({ coupon });
+    if (coupon !== undefined) computeOrderList({ coupon });
   }, [coupon, computeOrderList]);
 
   useEffect(() => {
-    if (points) computeOrderList({ points });
+    if (points !== undefined) computeOrderList({ points });
   }, [points, computeOrderList]);
 
   return null;
